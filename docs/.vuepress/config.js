98d195f8edb5eee8bce8f7efe167f2e89f38fed1
羽波 <31097812+SonodaHanami@users.noreply.github.com>
2024-06-12 01:17:18 +0900

    Update config.js
    add Google Analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,16 @@ export default defineUserConfig({
   base: '/yubot-docs/',
   title: '羽bot',
   description: '文档',
+  head: [
+    ['script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-MZCWWQJQQT' }],
+    [
+      'script', {}, `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-MZCWWQJQQT');
+      `
+    ],
+  ],
 })
 
